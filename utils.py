c8643ae4db09954c00d2fbc5a516d91207e8d9de
yeswanth <bata@softwareag.com>
2024-04-10 12:36:30 +0530

    adding models

--- utils.py
+++ utils.py
@@ -10,7 +10,6 @@ from pytorch_grad_cam.utils.image import show_cam_on_image
 
 
 def lr_finder(train_loader,optimizer,criterion,model,end_lr):
-    criterion = nn.CrossEntropyLoss()
     lr_finder = LRFinder(model, optimizer, criterion, device="mps")
     lr_finder.range_test(train_loader,end_lr=end_lr, num_iter=100, step_mode="exp")
     lr_finder.plot(log_lr=False)
