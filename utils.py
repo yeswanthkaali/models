43d7410cb49140b7b0f3872e190685c5ce107385
yeswanth <bata@softwareag.com>
2024-04-10 12:32:09 +0530

    adding models

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def lr_finder(train_loader,optimizer,criterion,model,end_lr):
 from tqdm import tqdm
 def GetCorrectPredCount(pPrediction, pLabels):
   return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
-def train(model, device, train_loader, optimizer,scheduler):
+def train(model, device, train_loader, optimizer,scheduler,criterion):
     model.train()
     pbar = tqdm(train_loader)
     train_loss = 0
@@ -28,7 +28,7 @@ def train(model, device, train_loader, optimizer,scheduler):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
         scheduler.step()
@@ -41,7 +41,7 @@ def train(model, device, train_loader, optimizer,scheduler):
     return train_loss,train_accuracy
 
 
-def test(model, device, test_loader):
+def test(model, device, test_loader,criterion):
     model.eval()
     test_loss = 0
     correct = 0
@@ -49,7 +49,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
